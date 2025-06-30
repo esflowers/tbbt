9b876df44c7d6aa4c12ac4364369365c046a62c3
esflowers <estrada.flsw@gmail.com>
2025-06-29 19:13:09 -0600

    create types file

---
+++ src/types.d.ts
@@ -0,0 +1,29 @@
+type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N ? Acc[number] : Enumerate<N, [...Acc, Acc['length']]>
+type BuildRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>> | F
+type DD = BuildRange<1, 32>
+type MM = BuildRange<1, 13>
+type YY = '07' | '08' | '09' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19'
+type Date = `${DD}.${MM}.${YY}`
+
+export type Link = {
+    title: string,
+    link: string,
+    label?: string
+}
+
+export type Episode = {
+    number: number,
+    title: string,
+    release: Date,
+    link: string
+}
+
+export type Season = {
+    number: number,
+    title: string,
+    link: string,
+    start: Date,
+    end: Date,
+    nEpisodes: number,
+    episodes: Episode[]
+}
