6c12050e10b40af16404306faeb8d3dacaacac42
esflowers <estrada.flsw@gmail.com>
2025-06-29 19:13:40 -0600

    create basic seasons file

---
+++ src/data/seasons.ts
@@ -0,0 +1,354 @@
+import type { Season } from '../types'
+
+export const seasons: Season[] = [
+    {
+        number: 1,
+        title: 'Temporada 1',
+        link: '/seasons/1',
+        start: '24.9.07',
+        end: '19.5.08',
+        nEpisodes: 17,
+        episodes: [
+            {
+                number: 1,
+                title: 'Piloto',
+                release: '24.9.07',
+                link: ''
+            },
+            {
+                number: 2,
+                title: 'La hipótesis del gran cereal',
+                release: '1.10.07',
+                link: ''
+            },
+            {
+                number: 3,
+                title: 'El corolario de Botas Peludas',
+                release: '8.10.07',
+                link: ''
+            },
+            {
+                number: 4,
+                title: 'El efecto del pez luminoso',
+                release: '15.10.07',
+                link: ''
+            },
+            {
+                number: 5,
+                title: 'El postulado de la hamburguesa',
+                release: '22.10.07',
+                link: ''
+            },
+            {
+                number: 6,
+                title: 'El paradigma de la Tierra Media',
+                release: '29.10.07',
+                link: ''
+            },
+            {
+                number: 7,
+                title: 'La paradoja del Wan-Tun',
+                release: '5.11.07',
+                link: ''
+            },
+            {
+                number: 8,
+                title: 'El experimento del Grasshopper',
+                release: '12.11.07',
+                link: ''
+            },
+            {
+                number: 9,
+                title: 'La polarización Cooper-Hofstadter',
+                release: '17.3.08',
+                link: ''
+            },
+            {
+                number: 10,
+                title: 'La resultante de Loobenfeld',
+                release: '24.3.08',
+                link: ''
+            },
+            {
+                number: 11,
+                title: 'La anomalía de la masa de tortitas',
+                release: '31.3.08',
+                link: ''
+            },
+            {
+                number: 12,
+                title: 'La dualidad de Jerusalén',
+                release: '14.4.08',
+                link: ''
+            },
+            {
+                number: 13,
+                title: 'La conjetura del Bat-tarro',
+                release: '21.4.08',
+                link: ''
+            },
+            {
+                number: 14,
+                title: 'La aniquilación del paraíso frikinal',
+                release: '26.4.08',
+                link: ''
+            },
+            {
+                number: 15,
+                title: 'La indeterminación de la chuleta de cerdo',
+                release: '17.3.08',
+                link: ''
+            },
+            {
+                number: 16,
+                title: 'La reacción del cacahuate',
+                release: '12.5.08',
+                link: ''
+            },
+            {
+                number: 17,
+                title: 'El factor mandarina',
+                release: '19.5.08',
+                link: ''
+            },
+        ]
+    },
+    {
+        number: 2,
+        title: 'Temporada 2',
+        link: '/seasons/2',
+        start: '22.9.08',
+        end: '11.5.09',
+        nEpisodes: 23,
+        episodes: [
+            {
+                number: 18,
+                title: 'El Paradigma del Pescado Malo',
+                release: '22.9.08',
+                link: ''
+            },
+            {
+                number: 19,
+                title: 'La Topología de la Coquilla',
+                release: '29.9.08',
+                link: ''
+            },
+            {
+                number: 20,
+                title: 'La Sublimación de los Bárbaros',
+                release: '6.10.08',
+                link: ''
+            },
+            {
+                number: 21,
+                title: 'La Equivalencia de Griffin',
+                release: '13.10.08',
+                link: ''
+            },
+            {
+                number: 22,
+                title: 'La Variante de Euclides',
+                release: '20.10.08',
+                link: ''
+            },
+            {
+                number: 23,
+                title: 'El Teorema de Cooper-Nowitzki',
+                release: '3.11.08',
+                link: ''
+            },
+            {
+                number: 24,
+                title: 'La Polarización Panty Piñata',
+                release: '10.11.08',
+                link: ''
+            },
+            {
+                number: 25,
+                title: 'La Expansión de Lagarto-Spock',
+                release: '17.11.08',
+                link: ''
+            },
+            {
+                number: 26,
+                title: 'La Triangulación de los Espárragos Blancos',
+                release: '24.11.08',
+                link: ''
+            },
+            {
+                number: 27,
+                title: 'El Rompecabezas Vartabedian',
+                release: '8.12.08',
+                link: ''
+            },
+            {
+                number: 28,
+                title: 'La Hipótesis del Regalo del Artículo de Baño',
+                release: '15.12.08',
+                link: ''
+            },
+            {
+                number: 29,
+                title: 'La Inestabilidad del Robot Asesino',
+                release: '12.1.09',
+                link: ''
+            },
+            {
+                number: 30,
+                title: 'El Algoritmo de la Amistad',
+                release: '19.1.09',
+                link: ''
+            },
+            {
+                number: 31,
+                title: 'La Permeabilidad Financiera',
+                release: '2.2.09',
+                link: ''
+            },
+            {
+                number: 32,
+                title: 'La Capacitancia Maternal',
+                release: '9.2.09',
+                link: ''
+            },
+            {
+                number: 33,
+                title: 'La Saturación del Cojín',
+                release: '2.3.09',
+                link: ''
+            },
+            {
+                number: 34,
+                title: 'La Separación de Terminator',
+                release: '9.3.09',
+                link: ''
+            },
+            {
+                number: 35,
+                title: 'El Nanoclúster de la Canción de Trabajo',
+                release: '16.3.09',
+                link: ''
+            },
+            {
+                number: 36,
+                title: 'La Yuxtaposición de la Prostituta Muerta',
+                release: '30.3.09',
+                link: ''
+            },
+            {
+                number: 37,
+                title: 'El Isótopo Hofstadter',
+                release: '13.4.09',
+                link: ''
+            },
+            {
+                number: 38,
+                title: 'La Renormalización de Las Vegas',
+                release: '27.4.09',
+                link: ''
+            },
+            {
+                number: 39,
+                title: 'La Turbulencia de los Materiales Clasificados',
+                release: '4.5.09',
+                link: ''
+            },
+            {
+                number: 40,
+                title: 'La Expedición Monopolar',
+                release: '11.5.09',
+                link: ''
+            },
+        ]
+    },
+    {
+        number: 3,
+        title: 'Temporada 3',
+        link: '/seasons/3',
+        start: '21.9.09',
+        end: '24.5.10',
+        nEpisodes: 23,
+        episodes: []
+    },
+    {
+        number: 4,
+        title: 'Temporada 4',
+        link: '/seasons/4',
+        start: '23.9.10',
+        end: '19.5.11',
+        nEpisodes: 24,
+        episodes: []
+    },
+    {
+        number: 5,
+        title: 'Temporada 5',
+        link: '/seasons/5',
+        start: '22.9.11',
+        end: '10.5.12',
+        nEpisodes: 24,
+        episodes: []
+    },
+    {
+        number: 6,
+        title: 'Temporada 6',
+        link: '/seasons/6',
+        start: '27.9.12',
+        end: '16.5.13',
+        nEpisodes: 24,
+        episodes: []
+    },
+    {
+        number: 7,
+        title: 'Temporada 7',
+        link: '/seasons/7',
+        start: '26.9.13',
+        end: '15.5.14',
+        nEpisodes: 24,
+        episodes: []
+    },
+    {
+        number: 8,
+        title: 'Temporada 8',
+        link: '/seasons/8',
+        start: '22.9.14',
+        end: '7.5.15',
+        nEpisodes: 24,
+        episodes: []
+    },
+    {
+        number: 9,
+        title: 'Temporada 9',
+        link: '/seasons/9',
+        start: '21.9.15',
+        end: '12.5.16',
+        nEpisodes: 24,
+        episodes: []
+    },
+    {
+        number: 10,
+        title: 'Temporada 10',
+        link: '/seasons/10',
+        start: '19.9.16',
+        end: '11.5.17',
+        nEpisodes: 24,
+        episodes: []
+    },
+    {
+        number: 11,
+        title: 'Temporada 11',
+        link: '/seasons/11',
+        start: '25.9.17',
+        end: '10.5.18',
+        nEpisodes: 24,
+        episodes: []
+    },
+    {
+        number: 12,
+        title: 'Temporada 12',
+        link: '/seasons/12',
+        start: '24.9.18',
+        end: '16.5.19',
+        nEpisodes: 24,
+        episodes: []
+    }
+]
