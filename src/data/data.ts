4f922a7c6e9abcc0d5816b6c3315c56af9d82f5c
esflowers <estrada.flsw@gmail.com>
2025-06-29 19:13:26 -0600

    create data file

---
+++ src/data/data.ts
@@ -0,0 +1,47 @@
+import type { Link } from "../types"
+
+export const name = 'the BiG Bang Theory'
+export const tagline = 'Sitcom estadounidense'
+export const title= 'The Big Bang Theory. Sitcom. Serie en Español Latino.'
+export const description = 'The Big Bang Theory es una comedia de situación estadounidense creada por Chuck Lorre y Bill Prady, quienes se desempeñaron como productores ejecutivos y escritores principales de la serie, junto con Steven Molaro.'
+export const preview = 'https://tbbt.esflowers.icu/preview.png'
+
+export const creator: Link = {
+    title: 'esflowers',
+    link: 'https://esflowers.icu',
+    label: 'Creador de este sitio web'
+}
+
+export const site: Link = {
+    title: name,
+    link: 'https://tbbt.esflowers.icu',
+    label: 'tbbt.esflowers.icu'
+}
+
+export const bmc: Link = {
+    title: 'Buy Me a Coffee',
+    link: 'https://coff.ee/esflowers_',
+    label: 'Contribuye al mantenimiento de este sitio web'
+}
+
+export const wiki: Link = {
+    title: 'Wikipedia',
+    link: 'https://es.wikipedia.org/wiki/The_Big_Bang_Theory',
+    label: 'Información de la serie'
+}
+
+export const provider: Link = {
+    title: 'proveedor',
+    link: 'https://thebigbangtheorytvlatinoo.blogspot.com/',
+    label: 'https://facebook.com/chinotvserie/'
+}
+
+export const serie = {
+    creator: 'Chuck Lorre y Bill Prady',
+    genre: 'Sitcom',
+    seasons: 12,
+    episodes: 279,
+    start: '24.09.2007',
+    end: '16.05.2019',
+    site: 'https://www.cbs.com/shows/big_bang_theory'
+}
